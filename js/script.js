454479521597c5f9135ede036afcfcfcc13bb336
Vyacheslav <kanto72@yandex.ru>
2021-03-19 15:51:30 +0500

    svetlana

--- js/script.js
+++ js/script.js
@@ -1,55 +1,28 @@
+function openDrop(el) {
+    $(el).find('.unknow-drop').addClass('unknow-drop__active');
+    $(el).find('.rotate').addClass('rotate__active');
 
-class Slider {
-    constructor (rangeElement, valueElement, options) {
-      this.rangeElement = rangeElement
-      this.valueElement = valueElement
-      this.options = options
-  
-      // Attach a listener to "change" event
-      this.rangeElement.addEventListener('input', this.updateSlider.bind(this))
-    }
-  
-    // Initialize the slider
-    init() {
-      this.rangeElement.setAttribute('min', options.min)
-      this.rangeElement.setAttribute('max', options.max)
-      this.rangeElement.value = options.cur
-  
-      this.updateSlider()
-    }
-  
-    // Format the money
-    asMoney(value) {
-      return '$' + parseFloat(value)
-        .toLocaleString('en-US', { maximumFractionDigits: 2 })
-    }
-  
-    generateBackground(rangeElement) {   
-      if (this.rangeElement.value === this.options.min) {
-        return
-      }
-  
-      let percentage =  (this.rangeElement.value - this.options.min) / (this.options.max - this.options.min) * 100
-      return 'background: linear-gradient(to right, #50299c, #7a00ff ' + percentage + '%, #d3edff ' + percentage + '%, #dee1e2 100%)'
-    }
-  
-    updateSlider (newValue) {
-      this.valueElement.innerHTML = this.asMoney(this.rangeElement.value)
-      this.rangeElement.style = this.generateBackground(this.rangeElement.value)
-    }
-  }
-  
-  let rangeElement = document.querySelector('.range [type="range"]')
-  let valueElement = document.querySelector('.range .range__value input') 
-  
-  let options = {
-    min: 0,
-    max: 750000,
-    cur: 37500
-  }
-  
-  if (rangeElement) {
-    let slider = new Slider(rangeElement, valueElement, options)
-  
-    slider.init()
-  }
+}
+$(document).mouseup(function (e) {
+var container = $(".unknow-drop");
+if (container.has(e.target).length === 0){
+    $('.unknow-drop').removeClass('unknow-drop__active');
+    $('.rotate').removeClass('rotate__active');
+}
+});
+$(document).ready(function() {
+    $(".fancybox").fancybox();
+});
+
+function openPopup() {
+    $('.popup').addClass('popup__active');
+    $('body').addClass('popup-active')
+    $(document).mouseup(function (e) {
+        var container = $(".popup");
+        if (container.has(e.target).length === 0){
+            $('.popup').removeClass('popup__active');
+            $('body').removeClass('popup-active')
+        }
+    });
+}
+
